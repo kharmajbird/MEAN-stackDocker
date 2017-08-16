138bc3e856f43bd241d8db8affbd2d8521cf2556
James Lewis <kharmajbird@gmail.com>
2017-08-15 20:35:08 -0400

    pull compose file for mean demo from git, just like the stack compose file; update Skelator README

--- 00-init.sh
+++ 00-init.sh
@@ -7,6 +7,7 @@ echo "docker-machine ssh swarm-1"
 echo "docker network create --driver overlay proxy"
 
 echo "curl -o docker-compose-stack.yml https://raw.githubusercontent.com/vfarcic/docker-flow-proxy/master/docker-compose-stack.yml"
+echo "curl -o docker-compose-mean-demo.yml https://raw.githubusercontent.com/kharmajbird/MEAN-stackDocker/master/docker-compose-mean-demo.yml"
 
 echo "docker stack deploy -c docker-compose-stack.yml proxy"
 echo "docker stack deploy -c docker-compose-mean-demo.yml meany"
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MEAN-stackDocker
+
+00-init.sh
+
+eval $(docker-machine env swarm-1)
+
+open "http://$(docker-machine ip swarm-1)/demo"
