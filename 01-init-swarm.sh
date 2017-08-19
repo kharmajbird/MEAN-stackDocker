9f54c369bb82671dd3afe4c78dd93c5bbd0dde0c
James Lewis <kharmajbird@gmail.com>
2017-08-19 18:05:41 -0400

    switch to a custom stack config; first stab at introducing a docker stack of elk

--- 01-init-swarm.sh
+++ 01-init-swarm.sh
@@ -2,11 +2,22 @@
 
 ./dm-swarm.sh
 
-docker-machine ssh swarm-1 \
-    docker network create --driver overlay proxy
+mkdir -p /tmp/docker/logstash
+
+cat << EOF > /tmp/docker/logstash/logstash.conf
+input {
+  syslog { port => 51415 }
+}
+EOF
+
 
 docker-machine ssh swarm-1 \
-    curl -o docker-compose-stack.yml https://raw.githubusercontent.com/vfarcic/docker-flow-proxy/master/docker-compose-stack.yml
+    docker network create --driver overlay proxy
+docker-machine ssh swarm-1 \
+    docker network create --driver overlay elk
+
+docker-machine ssh swarm-1 \
+    curl -o docker-compose-stack.yml https://raw.githubusercontent.com/kharmajbird/MEAN-stackDocker/master/docker-compose-stack.yml
 docker-machine ssh swarm-1 \
     curl -o docker-compose-mean-demo.yml https://raw.githubusercontent.com/kharmajbird/MEAN-stackDocker/master/docker-compose-mean-demo.yml
 
