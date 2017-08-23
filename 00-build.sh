bad8a1dc868f4ddfbe769d160397512222c9ada1
James Lewis <kharmajbird@gmail.com>
2017-08-23 16:50:06 -0400

    open up kibana to the proxy; clean up docker tagging

--- 00-build.sh
+++ 00-build.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 (cd docker/meany && docker build -t meany .) && \
-(cd docker/logstash && docker build -t logstash:2.4 .) && \
+(cd docker/logstash && docker build -t kharmajbird/logstash:2.4 .) && \
 \
 docker tag meany kharmajbird/meany && \
-docker tag logstash kharmajbird/logstash:2.4 && \
 \
 docker login && \
 docker push kharmajbird/meany && \
