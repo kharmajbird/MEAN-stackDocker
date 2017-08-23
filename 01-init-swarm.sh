f3679414aa2a7040ae58f07b82f91ea9ad465f70
James Lewis <kharmajbird@gmail.com>
2017-08-23 15:55:52 -0400

    added logspout service to startup; removed << secrets work; added meany to elk network

--- 01-init-swarm.sh
+++ 01-init-swarm.sh
@@ -2,24 +2,6 @@
 
 ./dm-swarm.sh
 
-mkdir -p /tmp/docker/logstash
-
-cat << EOF > /tmp/docker/logstash/logstash.conf
-input {
-  syslog { port => 51415 }
-}
-
-output {
-  elasticsearch {
-    hosts => ["elasticsearch:9200"]
-  }
-  # Remove in production
-  stdout {
-    codec => rubydebug
-  }
-}
-EOF
-
 docker-machine ssh swarm-1 \
     docker network create --driver overlay proxy
 docker-machine ssh swarm-1 \
@@ -31,6 +13,8 @@ docker-machine ssh swarm-1 \
     curl -o docker-compose-elk.yml https://raw.githubusercontent.com/kharmajbird/MEAN-stackDocker/master/docker-compose-elk.yml
 docker-machine ssh swarm-1 \
     curl -o docker-compose-mean-demo.yml https://raw.githubusercontent.com/kharmajbird/MEAN-stackDocker/master/docker-compose-mean-demo.yml
+docker-machine ssh swarm-1 \
+    curl -o docker-compose-logspout.yml https://raw.githubusercontent.com/kharmajbird/MEAN-stackDocker/master/docker-compose-logspout.yml
 
 docker-machine ssh swarm-1 \
     docker stack deploy -c docker-compose-proxy.yml proxy
@@ -38,3 +22,5 @@ docker-machine ssh swarm-1 \
     docker stack deploy -c docker-compose-mean-demo.yml meany
 docker-machine ssh swarm-1 \
     docker stack deploy -c docker-compose-elk.yml elk
+docker-machine ssh swarm-1 \
+    docker stack deploy -c docker-compose-logspout.yml logspout
