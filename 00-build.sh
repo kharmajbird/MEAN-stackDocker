9bf1f99d27543898fe97df442579f815afc10ac7
James Lewis <kharmajbird@gmail.com>
2017-08-23 16:22:13 -0400

    retag local logstash image and apply it; remove volume and secret nonsense

--- 00-build.sh
+++ 00-build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-docker build -t meany . && \
+(cd docker/meany && docker build -t meany .) && \
+(cd docker/logstash && docker build -t logstash:2.4 .) && \
+\
 docker tag meany kharmajbird/meany && \
-docker login
-docker push kharmajbird/meany
+docker tag logstash kharmajbird/logstash:2.4 && \
+\
+docker login && \
+docker push kharmajbird/meany && \
+docker push kharmajbird/logstash
