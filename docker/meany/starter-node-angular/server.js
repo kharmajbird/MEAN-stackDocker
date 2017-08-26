a7a438922bfced16a4b103977583c1092135f783
James Lewis <kharmajbird@gmail.com>
2017-08-25 20:06:22 -0400

    all that is needed for modern-syslog is console.log()?

--- docker/meany/starter-node-angular/server.js
+++ docker/meany/starter-node-angular/server.js
@@ -26,15 +26,13 @@ app.use(express.static(__dirname + '/public')); // set the static files location
 // routes ==================================================
 require('./app/routes')(app); // pass our application into our routes
 
-Syslog.init("modern-syslog", Syslog.LOG_PID | Syslog.LOG_ODELAY, Syslog.LOG_LOCAL0);
-Syslog.log(Syslog.LOG_ERR, "Magic Johnson happens on port " + port + " and port 32... 10 more and you will have the secret to the universe.  " + new Date());
-Syslog.close();
+// Syslog.init("modern-syslog", Syslog.LOG_PID | Syslog.LOG_ODELAY, Syslog.LOG_LOCAL0);
+// Syslog.log(Syslog.LOG_ERR, "Magic Johnson happens on port " + port + " and port 32... 10 more and you will have the secret to the universe.  " + new Date());
+// Syslog.close();
 
 // start app ===============================================
 app.listen(port);	
 
-
-console.log("console Magic Johnson happens on port " + port + " and port 32... 10 more and you will have the secret to the universe.  " + new Date());
-console.log("TEST");
+console.log("Magic Johnson happens on port " + port + " and port 32... 10 more and you will have the secret to the universe.  " + new Date());
 
 exports = module.exports = app; 						// expose app
