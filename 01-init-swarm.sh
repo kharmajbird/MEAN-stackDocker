194d0d1505aba1cd2d4846e89ca158d99eff695f
James Lewis <kharmajbird@gmail.com>
2017-09-03 15:39:09 -0400

    meany stack should not run on elk nodes, for better balancing

--- 01-init-swarm.sh
+++ 01-init-swarm.sh
@@ -7,6 +7,11 @@ docker-machine ssh swarm-1 \
 docker-machine ssh swarm-1 \
     docker network create --driver overlay elk
 
+docker node update --label-add nonlogger=true swarm-1
+docker node update --label-add nonlogger=true swarm-2
+docker node update --label-add nonlogger=true swarm-3
+docker node update --label-add nonlogger=true swarm-4
+docker node update --label-add nonlogger=true swarm-5
 docker node update --label-add logger=true swarm-6
 docker node update --label-add logger=true swarm-7
 
