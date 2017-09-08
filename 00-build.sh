b2f98253dcd2fec9256297d9e019b59bee81b618
James Lewis <kharmajbird@gmail.com>
2017-09-08 14:14:22 -0400

    first stab at mongo replica sets

--- 00-build.sh
+++ 00-build.sh
@@ -3,8 +3,10 @@
 (cd docker/meany && docker build -t kharmajbird/meany:latest .) && \
 (cd docker/logstash && docker build -t kharmajbird/logstash:latest .) && \
 (cd docker/go-demo && docker build -f Dockerfile.big -t kharmajbird/go-demo:latest .) && \
+(cd docker/mongo && docker build -t kharmajbird/mongo:latest .) && \
 \
 echo docker login && \
 docker push kharmajbird/meany:latest && \
 docker push kharmajbird/logstash:latest && \
-docker push kharmajbird/go-demo:latest
+docker push kharmajbird/go-demo:latest && \
+docker push kharmajbird/mongo:latest
---
+++ docker/mongo/init-mongo-rs.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#for rs in "$@"; do
+for rs in "$SETS"; do
+    mongo --host $rs --eval 'db'
+    while [ $? -ne 0 ]; do
+        echo "Waiting for $rs to become available"
+        sleep 3
+        mongo --host $rs --eval 'db'
+    done
+done
+
+i=0
+for rs in "$SETS"; do
+    if [ "$rs" != "$1" ]; then
+        MEMBERS="$MEMBERS ,"
+    fi
+    MEMBERS="$MEMBERS {_id: $i, host: \"$rs\" }"
+    i=$((i+1))
+done
+
+mongo --host $1 --eval "rs.initiate({_id: \"rs0\", version: 1, members: [$MEMBERS]})"
+sleep 3
+mongo --host $1 --eval 'rs.status()'
